3c83e6c7bacdc0abff0b706285b990cf9b567aef
Adam Lamoglia <adamrammansee@hotmail.co>
2019-09-26 19:37:10 -0300

    create keystore file / put sentry report of errors in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,37 @@
 import 'package:app/presentation/app.dart';
 import 'package:flutter/material.dart';
+import 'package:sentry/sentry.dart';
+import 'dart:async';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
+final SentryClient _sentry = SentryClient(
+    dsn: "https://2173bb44d75b4617b22e11ddcd4b9345@sentry.io/1760063");
+
+
+Future<void> _reportError(dynamic error, dynamic stackTrace) async {
+
+  print('Caught error: $error');
+
+  _sentry.captureException(
+    exception: error,
+    stackTrace: stackTrace,
+  );
 }
+
+Future<Null> main() async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+  };
+
+  runZoned<Future<Null>>(() async {
+    runApp(new MyApp());
+  }, onError: (error, stackTrace) async {
+    await _reportError(error, stackTrace);
+
+    exit(0);
+  });
+}
+
+
+
+
