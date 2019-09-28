7f43c1b9e96661151444f08a0de3662447a22812
Daniel Peixoto <danielspeixoto@outlook.com>
2019-09-28 10:37:55 -0300

    handles tournament, training and challenge

--- lib/presentation/background/FirebaseNotifications.dart
+++ lib/presentation/background/FirebaseNotifications.dart
@@ -1,8 +1,25 @@
 import 'dart:io';
 
+import 'package:app/presentation/state/actions/ChallengeActions.dart';
+import 'package:app/presentation/state/actions/ENEMActions.dart';
 import 'package:app/presentation/state/app_state.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:redux/redux.dart';
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
+  print("background notification handler");
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
+}
 
 class FirebaseNotifications {
   FirebaseMessaging _firebaseMessaging;
@@ -13,18 +30,31 @@ class FirebaseNotifications {
     if (Platform.isIOS) iOSPermission();
 
     _firebaseMessaging.getToken().then((token) {
-       print(token);
+      print(token);
     });
 
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
-        print('on message $message');
+        print('on notification message $message');
       },
+      onBackgroundMessage: myBackgroundMessageHandler,
       onResume: (Map<String, dynamic> message) async {
-        print('on resume $message');
+        print("notification: on resume");
+        if (store.state.loginState.isUserLoggedIn()) {
+          if (message["data"]["action"] == "start_challenge") {
+            store.dispatch(ChallengeAction());
+          }
+          if (message["data"]["action"] == "enem_training") {
+            store.dispatch(TrainingAction());
+          }
+          if (message["data"]["action"] == "enem_tournament") {
+            store.dispatch(TournamentAction());
+          }
+        }
       },
       onLaunch: (Map<String, dynamic> message) async {
-        print('on launch $message');
+        print('notification: on launch $message');
+        store.dispatch(ChallengeAction());
       },
     );
   }
--- lib/presentation/state/reducers/LoginReducer.dart
+++ lib/presentation/state/reducers/LoginReducer.dart
@@ -4,21 +4,25 @@ import 'package:redux/redux.dart';
 import 'Data.dart';
 
 class LoginState {
-  final Data<bool> isLoggedIn;
+  final Data<bool> loginStatus;
 
-  LoginState(this.isLoggedIn);
+  LoginState(this.loginStatus);
 
   factory LoginState.initial() => LoginState(
-    Data(),
-  );
+        Data(),
+      );
 
   LoginState copyWith({
     Data<bool> isLoggedIn,
   }) {
     final state = LoginState(
-      isLoggedIn ?? this.isLoggedIn,
+      isLoggedIn ?? this.loginStatus,
     );
     return state;
+  }
+
+  bool isUserLoggedIn() {
+    return loginStatus.content != null && loginStatus.content;
   }
 }
 
@@ -31,13 +35,13 @@ final Reducer<LoginState> loginReducer = combineReducers<LoginState>([
 
 LoginState loginSuccess(LoginState state, LoginSuccessAction action) {
   return state.copyWith(
-    isLoggedIn: state.isLoggedIn.copyWith(content: true, isLoading: false),
+    isLoggedIn: state.loginStatus.copyWith(content: true, isLoading: false),
   );
 }
 
 LoginState loginFailed(LoginState state, NotLoggedInAction action) {
   return state.copyWith(
-    isLoggedIn: state.isLoggedIn.copyWith(content: false, isLoading: false),
+    isLoggedIn: state.loginStatus.copyWith(content: false, isLoading: false),
   );
 }
 
