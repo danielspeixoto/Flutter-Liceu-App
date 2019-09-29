2c568030102d931fbbf457ba25670c47eb339a1b
Daniel Peixoto <danielspeixoto@outlook.com>
2019-09-29 01:22:20 -0300

    shows opponent picture and name when playing against him in a challenge (#8)

--- lib/presentation/state/aggregates/ChallengeData.dart
+++ lib/presentation/state/aggregates/ChallengeData.dart
@@ -1,11 +1,17 @@
+import 'package:app/domain/aggregates/User.dart';
+
 import 'TriviaData.dart';
 
 class ChallengeData {
   final String id;
+  final User challenger;
+  final User challenged;
   final List<TriviaData> questions;
 
   ChallengeData(
     this.id,
     this.questions,
+    this.challenger,
+    this.challenged,
   );
 }
