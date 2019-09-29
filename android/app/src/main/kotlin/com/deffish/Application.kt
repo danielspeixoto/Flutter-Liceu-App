68d2537f6594d96ccaaf27091f25e5e430939d8c
Daniel Peixoto <danielspeixoto@outlook.com>
2019-09-29 00:14:24 -0300

    notifications works when app is closed

--- android/app/src/main/kotlin/com/deffish/Application.kt
+++ android/app/src/main/kotlin/com/deffish/Application.kt
@@ -1,10 +1,12 @@
 package com.deffish
 
+import android.util.Log
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
 
 class Application : FlutterApplication(), PluginRegistrantCallback {
     @Override
@@ -15,6 +17,7 @@ class Application : FlutterApplication(), PluginRegistrantCallback {
 
     @Override
     override fun registerWith(registry: PluginRegistry) {
-        GeneratedPluginRegistrant.registerWith(registry)
+//        GeneratedPluginRegistrant.registerWith(registry)
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"))
     }
 }
