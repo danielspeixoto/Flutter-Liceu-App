5b1816454924757cfca92d7988ae6d1f590a6956
Adam Lamoglia <adamrammansee@hotmail.co>
2019-10-09 14:48:06 -0300

    [CHANGE] replace report button in training e tournament-review

--- lib/presentation/pages/tournament-review/ViewModel.dart
+++ lib/presentation/pages/tournament-review/ViewModel.dart
@@ -1,3 +1,4 @@
+import 'package:app/presentation/state/actions/ReportActions.dart';
 import 'package:app/presentation/state/aggregates/ENEMQuestionData.dart';
 import 'package:app/presentation/state/app_state.dart';
 import 'package:app/presentation/state/reducers/Data.dart';
@@ -7,11 +8,13 @@ class TournamentReviewViewModel {
   final Data<List<ENEMQuestionData>> questions;
   final int timeSpent;
   final int score;
+  final Function(String questionId, int correctAnswer) onReportButtonPressed;
 
   TournamentReviewViewModel({
     this.timeSpent,
     this.score,
     this.questions,
+    this.onReportButtonPressed
   });
 
   factory TournamentReviewViewModel.create(Store<AppState> store) {
@@ -20,6 +23,17 @@ class TournamentReviewViewModel {
       questions: enemState.tournamentQuestions,
       score: enemState.score,
       timeSpent: enemState.timeSpent,
+      onReportButtonPressed: (String questionId, int correctAnswer) {
+
+          Map<String, dynamic> params = {
+            "questionId": questionId,
+            "correctAnswer: ": correctAnswer
+          };
+
+          List<String> tags = ["generated", "enem", "question", "incorrect", "answer"];
+          store.dispatch(
+              SubmitReportAction("O gabarito da questão não está correto.", tags, params));
+        }
     );
   }
 }
--- lib/presentation/pages/tournament/ViewModel.dart
+++ lib/presentation/pages/tournament/ViewModel.dart
@@ -9,13 +9,12 @@ class TournamentViewModel {
   final Data<List<ENEMQuestionData>> questions;
   final Function(String questionId, int answer) onAnswer;
   final Function() submit;
-  final Function(String questionId, int correctAnswer) onReportButtonPressed;
+  
 
   TournamentViewModel({
     this.questions,
     this.onAnswer,
     this.submit,
-    this.onReportButtonPressed
   });
 
   factory TournamentViewModel.create(Store<AppState> store) {
@@ -23,17 +22,7 @@ class TournamentViewModel {
         questions: store.state.enemState.tournamentQuestions,
         onAnswer: (String questionId, int answer) =>
             store.dispatch(AnswerTournamentQuestionAction(questionId, answer)),
-        submit: () => store.dispatch(EndTournamentGameAction()),
-        onReportButtonPressed: (String questionId, int correctAnswer) {
-
-          Map<String, dynamic> params = {
-            "questionId": questionId,
-            "correctAnswer: ": correctAnswer
-          };
-
-          List<String> tags = ["generated", "enem", "question", "incorrect", "answer"];
-          store.dispatch(
-              SubmitReportAction("O gabarito da questão não está correto.", tags, params));
-        });
+        submit: () => store.dispatch(EndTournamentGameAction()));
+        
   }
 }
