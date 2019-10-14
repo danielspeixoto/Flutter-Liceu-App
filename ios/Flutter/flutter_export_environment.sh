241f1dbc34eaac795556408fda015e016bab249e
Daniel Peixoto <danielspeixoto@outlook.com>
2019-10-14 10:09:21 -0300

    ok (#52)

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/danielspeixoto/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/danielspeixoto/IdeaProjects/liceuapp/app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/danielspeixoto/IdeaProjects/liceuapp/app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/danielspeixoto/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/danielspeixoto/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=3.0.20"
 export "FLUTTER_BUILD_NUMBER=201"
+export "TRACK_WIDGET_CREATION=true"
