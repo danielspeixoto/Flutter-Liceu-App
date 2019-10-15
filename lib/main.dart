92c390efb8d90d1bc45534b450585d945354b622
Daniel Peixoto <danielspeixoto@outlook.com>
2019-10-15 17:21:29 -0300

    works

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+import 'dart:async';
+
 import 'package:app/presentation/app.dart';
 import 'package:app/util/StackFilter.dart';
 import 'package:app/util/sentry.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sentry/sentry.dart';
-import 'package:app/injection.dart';
-import 'dart:async';
-import 'dart:io';
+
+Future<Null> main() async {
+  await DotEnv().load('.env');
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    Zone.current.handleUncaughtError(details.exception, details.stack);
+  };
+
+  runZoned<Future<Null>>(() async {
+    runApp(new MyApp());
+  }, onError: (error, stackTrace) async {
+    await _reportError(error, filter(stackTrace));
+  });
+}
 
 final sentry = Sentry();
 
@@ -15,22 +28,7 @@ Future<void> _reportError(dynamic error, dynamic stackTrace) async {
   final Event event = new Event(
       exception: error,
       stackTrace: stackTrace,
-      environment: enviroment,
+      environment: DotEnv().env['ENV'],
       level: SeverityLevel.error);
   await sentry.client.capture(event: event);
-}
-
-Future<Null> main() async {
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    Zone.current.handleUncaughtError(details.exception, details.stack);
-  };
-
-  
-
-  runZoned<Future<Null>>(() async {
-    runApp(new MyApp());
-  }, onError: (error, stackTrace) async {
-    
-    await _reportError(error, filter(stackTrace));
-  });
 }
