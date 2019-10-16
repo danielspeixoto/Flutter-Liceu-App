8c3a80ee5b9dcd872e1d28916827220673b8c563
Daniel Peixoto <danielspeixoto@outlook.com>
2019-10-16 13:00:27 -0300

    skips facebook login and adds permissions for iOS devices

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,4 +1,6 @@
 // Imports the Flutter Driver API.
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
@@ -10,6 +12,13 @@ void main() {
 
     // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
+      await Process.run('applesimutils', [
+        "--booted YES",
+        "--clearKeychain",
+        "--bundle com.deffish",
+        "--setPermissions",
+        "notifications=YES"
+      ]);
       driver = await FlutterDriver.connect();
     });
 
@@ -22,6 +31,11 @@ void main() {
 
     test('User can login', () async {
       await driver.tap(skipIntroFinder);
+
+      final facebookLoginFinder = find.byValueKey("facebookLogin");
+      await driver.tap(facebookLoginFinder);
+      await driver.waitForAbsent(facebookLoginFinder);
+      find.byValueKey("homePage");
     });
   });
 }
